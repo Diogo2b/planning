85b51dc36d9247c57e6b80b9ff3dc752b0a6254c
AlexisL2b <alexisl2b@outlook.fr>
2023-01-30 10:06:26 +0100

    fix: ajout toolbar calendrier

--- Views/calendriers/index.php
+++ Views/calendriers/index.php
@@ -6,8 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <script src='../fullcalendar/dist/index.global.js'></script>
-  <!-- <script src='[![](https://data.jsdelivr.com/v1/package/npm/@fullcalendar/interaction/badge)](https://www.jsdelivr.com/package/npm/@fullcalendar/interaction)'></script> -->
-  <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+  <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
+<link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>  
+<script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   <title>Document</title>
 
@@ -26,7 +27,7 @@
       load()
       var draggableEl = document.getElementById('external-events');
       var calendarEl = document.getElementById('calendrier');
-      var checkbox = document.getElementById('drop-remove');
+      
 
 
       new Draggable(draggableEl, {
@@ -99,17 +100,24 @@
 
             var calendar = new Calendar(calendarEl, {
               headerToolbar: {
-                height:"auto",
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay',
+               
+                left:'title',
+                center: false ,
+                
 
               },
+              buttonText:{
+                today: 'Aujourd\'hui'
+              
+              },
+              locale:'fr',
               height:"auto",
+              themeSystem: 'bootstrap5',
+              allDaySlot: false,
               slotMinTime: '08:00',
               slotMaxTime: '18:00',
               weekends: false,
-              initialView: 'timeGridWeek',
+              initialView: ($(window).width() < 765) ? 'basicDay':'timeGridWeek',
               editable: (isEditable==1) ? true : false,
               droppable: true,
               displayEventEnd: true,
@@ -125,6 +133,7 @@
                 console.log('aaa')
                 let end_receive = info.event.endStr
                 let start_receive = info.event.startStr
+               
 
 
 
@@ -232,6 +241,9 @@
 
 
               },
+              windowResize: function(info){
+                
+              }
 
 
             });
@@ -239,7 +251,7 @@
             Object.values(response.events).forEach(response => {
               console.log(response.events)
               calendar.addEvent({
-                title: ['title'],
+                title: response['title'],
                 start: response['start'],
                 end: response['end'],
                 id: response['id'],
@@ -280,12 +292,14 @@ function create_session(){
       let end_receive = $("#end_selector").val();
       const module = $("#fc-event-main");
       let id_module_receive = module.data('id');
+      let title_module = $("#fc-event-main").text()
 
       $.ajax({
         url: '/modal_CreatePost',
         dataType: 'JSON',
         type: 'POST',
         data: {
+          title: title_module,
           profs: selector_profs,
           salle: selector_salle,
           classe: selector_class,
@@ -405,7 +419,7 @@ function update_session(){
     <div id='calendrier' class='col-8'>
     </div>
     <!-- //Modal sur callBack EventReceiv -->
-    <div class="modal fade " id="MaModal" backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal fade " data-bs-backdrop="static" data-bs-keyboard="false" id="MaModal" backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
 
